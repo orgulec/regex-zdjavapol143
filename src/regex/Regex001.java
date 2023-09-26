a56ef29dc5c52df961e1e284142468c7fb15a19f
orgulec <oggilion@interia.pl>
2023-09-26 18:46:41 +0200

    Update Regex001

--- src/regex/Regex001.java
+++ src/regex/Regex001.java
@@ -5,11 +5,12 @@ import java.util.regex.Pattern;
 public class Regex001 {
     /*
     Napisz Regex, który sprawdza polskie numery telefonów komórkowych,
-    zaczynające się cyfrą 5.
+    zaczynające się cyfrą 5
+    Uwzględnij że na początku może ale nie musi być +48 !
      */
-        private static final Pattern PATTERN = Pattern.compile("5[0-9]{8}");
+    private static final Pattern PATTERN = Pattern.compile("(\\+48)?5[0-9]{8}");
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         /*
         Prawda dla tekstu: 515332123
         Prawda dla tekstu: 515336293
@@ -19,22 +20,25 @@ public class Regex001 {
         Fałsz dla tekstu: 712123
         Fałsz dla tekstu: 51123
          */
-            validate("515332123");
-            validate("515336293");
-            validate("415336293");
-            validate("615332123");
-            validate("715332123");
-            validate("712123");
-            validate("51123");
-        }
-
-        public static void validate(String text){
-            Matcher matcher = PATTERN.matcher(text);
-            if(matcher.matches()){
-                System.out.println("Prawda dla tekstu: " + text);
-            } else {
-                System.out.println("Fałsz dla tekstu: " + text);
-            }
+        validate("515332123");
+        validate("+48515332123");
+        validate("+48432122123");
+        validate("515336293");
+        validate("415336293");
+        validate("615332123");
+        validate("715332123");
+        validate("712123");
+        validate("51123");
+    }
+    public static void validate(String text){
+        Matcher matcher = PATTERN.matcher(text);
+        if(matcher.matches()){
+            System.out.println("Prawda dla tekstu: " + text);
+        } else {
+           System.out.println("Fałsz dla tekstu: " + text);
         }
     }
+
+
+}
 
